402bb908744e04a23e2718a6a1560c4dba264d77
Icedream <icedream2k9@die-optimisten.net>
2012-10-30 13:54:59 +0100

    Server ips. Just for a testing so that I can properly fix that.

--- website/index.php
+++ website/index.php
@@ -48,7 +48,7 @@ function gen_leds($titles)
 		<? menu(); ?>
 		<p style="padding: 20px 5px 5px 5px; font-size: 32px; font-family: Electrolize; display: none">
 			<span id="fdocol">fourdeltaone</span>.<?=$_GET["page"] ?>
-			<span style="font-size: 11px"><?=$_SERVER["SERVER_NAME"] ?></span>
+			<span style="font-size: 11px"><?=$_SERVER['SERVER_ADDR'] ?></span>
 		</p>
 		</div>
 	</div>
